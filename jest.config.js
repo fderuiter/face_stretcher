145bdcd4b618f1b0f76d9c5b56fe3b3c5a0dc92c
Frederick de Ruiter <fderuiter@brightresearchpartners.com>
2025-04-17 09:56:50 -0500

    feat: add transform configuration for JSX and TSX files in Jest

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,9 @@ module.exports = {
         '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js',
     },
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    transform: {
+        '^.+\\.[t|j]sx?$': 'babel-jest'
+    },
     transformIgnorePatterns: [
         '/node_modules/(?!three|@tensorflow)'
     ]
